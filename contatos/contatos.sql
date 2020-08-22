36315ba857f35bf51ab3ea2c9503162b814087ed
Lucas-Camilo <htslucasv@gmail.com>
2020-08-22 14:21:09 -0300

    atualizando banco de dados

--- contatos/contatos.sql
+++ contatos/contatos.sql
@@ -1,11 +1,11 @@
-﻿DROP DATABASE IF EXISTS contatos;
-CREATE DATABASE IF NOT EXISTS contatos;
-USE contatos;
-CREATE TABLE usuario (
-usuario varchar(08) NOT NULL PRIMARY KEY,
-senha   varchar(60) NOT NULL,
-nome    varchar(50) NOT NULL,
-cat     varchar(02) NOT NULL);
-insert into usuario values
-('admin', SHA1('abc'), 'Administrador', '00'),
-('usu01', SHA1('abc'), 'Usuário 01', '01');
+﻿  DROP DATABASE IF EXISTS contatos;
+  CREATE DATABASE IF NOT EXISTS contatos;
+  USE contatos;
+  CREATE TABLE usuario (
+  usuario varchar(08) NOT NULL PRIMARY KEY,
+  senha   varchar(60) NOT NULL,
+  nome    varchar(50) NOT NULL,
+  cat     varchar(02) NOT NULL); 
+  insert into usuario values
+  ('admin', SHA1('abc'), 'Administrador', '00'),
+  ('usu01', SHA1('abc'), 'Usuário 01', '01');
