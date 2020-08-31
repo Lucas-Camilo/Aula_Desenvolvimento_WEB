4b179aa7c0d1c28c401f6215808657123430bb8c
Lucas-Camilo <htslucasv@gmail.com>
2020-08-31 20:59:31 -0300

    Inserindo insercao de contato

--- contatos/mcontatos.php
+++ contatos/mcontatos.php
@@ -55,7 +55,7 @@ if($op=="iic")
             PDO::ATTR_ERRMODE,
             PDO::ERRMODE_EXCEPTION
         );
-        $sth = $conec->prepare("INSERT INTO tipo values(?, ?, ?, ?)");
+        $sth = $conec->prepare("INSERT INTO contatosvalues(?, ?, ?, ?)");
         $sth->execute(array(
             $contato->getIdc(),
             $contato->getNomec(),
