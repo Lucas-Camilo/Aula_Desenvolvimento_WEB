895ea6dccbff1077d76add9604e5c717abeb70a0
Lucas-Camilo <htslucasv@gmail.com>
2020-08-31 18:57:28 -0300

    Correcao de erros

--- contatos/sistema.php
+++ contatos/sistema.php
@@ -10,7 +10,12 @@
            <a href='musuario.php?op=au'>Alterar</a>
                <a href='musuario.php?op=lu'>Listar</a>Usuário";
     }
-    print "<br> <a href='mtipo.php'>Inclusão de tipo </a>";
+    print "<br> <a href='mtipo.php?op=it'>Inclusão de tipo </a>
+        <a href='mtipo.php?op=it'>Novo</a>
+        <a href='mtipo.php?op=et'>Excluir</a>
+        <a href='mtipo.php?op=at'>Alterar</a>
+        <a href='mtipo.php?op=lt'>Listar</a>";
+
     print "<br> <a href='mcontatos.php'>Inclusão de tipo </a>";
     ?>
     <p align="center"><a href="index.php">Logoff</a></p>
